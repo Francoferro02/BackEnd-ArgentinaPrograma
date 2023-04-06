447b4a9487f661a515aea5519bc222f8b7a05388
Francoferro02 <francoandresferro@gmail.com>
2023-04-06 18:20:30 -0300

    Actualizacion

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM amazoncorretto:11-alpine-jdk 
+FROM amazoncorretto:8-alpine-jdk 
 MAINTAINER FRANCO
-COPY target/Franco-0.0.1-SNAPSHOT.jar Franco-Application.jar
+COPY target/Franco-0.0.1-SNAPSHOT.jar FrancoApplication.jar
 ENTRYPOINT ["java","-jar","/Franco-Application.jar"]
 EXPOSE 8080
--- src/main/java/com/portfolio/Franco/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/Franco/Controller/ExperienciaController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendfranco.web.app")
 public class ExperienciaController {
     @Autowired
     ServiceExperiencia sExperiencia;
